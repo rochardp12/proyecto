400731ea7c31a53be641dda5ef7c1068872c1d1b
unknown <ricluper@espol.edu.ec>
2021-11-30 22:46:23 -0500

    Creación de la clase Mascota

---
+++ src/main/java/ec/edu/espol/util/Util.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.util;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author eduardo
+ */
+public class Util {
+    
+    // el constructor se lo ha declarado privado
+    // ya que esta clase solo va a contener comportamientos estáticos
+    // por lo tanto, no se van a permitir crear instancia de la clase Util
+    private Util(){}
+    
+    public static int nextID(String nomfile)
+    {
+        int id = 0;
+        try(Scanner sc = new Scanner(new File(nomfile)))
+        {
+           while(sc.hasNextLine())
+           {
+               String linea = sc.nextLine();
+               String[] tokens = linea.split("\\|");
+               id = Integer.parseInt(tokens[0]);
+           }
+        }
+        catch(Exception e)
+        {
+        }
+        return id+1;
+    }
+    
+}
